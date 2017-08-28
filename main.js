d6779dde29eabf2b41e25e4aaa6e52d79941b1d9
pablo fuentes <pablofuentesg1@yahoo.es>
2017-08-28 13:53:23 +0200

    se mueve el div car arriba y abajo

--- main.js
+++ main.js
@@ -2,18 +2,33 @@ $(document).ready(function() {
 
 
 
-       setInterval(check , 20);
+       setInterval(check , 100);
 function check(){
-  goUp();
-if (document.onkeydown)
+
+($(document).keydown(function (e) {
+  console.log (e.keyCode);
+  if (e.keyCode===40)
+{  goDown();
+console.log("pasa");}
+  else if (e.keyCode===38)
+{  goUp();
+console.log("pasa2");}
+}));
+
 }
 
 function goUp(){
   var now = parseInt($(".car").css("top"));
-  now-=5;
+  now-=1;
   $(".car").css("top", now+"px");
 
 }
 
+function goDown(){
+  var now = parseInt($(".car").css("top"));
+  now+=1;
+  $(".car").css("top", now+"px");
+
+}
 
     });
